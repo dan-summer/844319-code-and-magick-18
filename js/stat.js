177ee316eff119162f212287a639f89350a30425
DanSummer <dan.summer363@gmail.com>
2019-09-17 13:53:23 +0300

    Update stat.js

--- js/stat.js
+++ js/stat.js
@@ -4,7 +4,7 @@ var CLOUD_WIDTH = 420; // ширина окна
 var CLOUD_HEIGHT = 270; // высота окна
 var CLOUD_X = 100; // координата окна по x
 var CLOUD_Y = 10; // координата окна по y
-var GAP = 10; // отступ внешний
+var GAP = 10; // внешний отступ
 var FONT_GAP = 30; // отступ текста
 var BAR_HEIGHT = 150; // высота столбика в гистограмме
 var BAR_WIDTH = 40; // ширина столбика в гистограмме
@@ -39,13 +39,10 @@ window.renderStatistics = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = '#000';
     ctx.fillText(names[i], CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP);
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'red';
-    } else {
-      ctx.fillStyle = 'blue';
-    }
     ctx.fillText(Math.round(times[i]), CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - FONT_GAP - GAP - (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
     ctx.fillRect(CLOUD_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - FONT_GAP - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
   }
 };
