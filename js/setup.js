607b8b0984dfa5db8a8c45879e49d74efbb598fb
DanSummer <dan.summer363@gmail.com>
2019-09-19 21:10:18 +0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -4,8 +4,9 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATS_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_COUNT = 4;
 
-// Окно-форма магов
+// Окно-выбора магов
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
@@ -17,38 +18,24 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-// случайное число-индекс
-var randomIndex = Math.floor(Math.random() * WIZARD_NAMES.length);
+// Функция генерации свойств мага (один элемент объекта)
+var getRandomElement = function (arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
 
-// Массив с объектами вида магов
-var wizards = [
-  {
-    name: WIZARD_NAMES[randomIndex],
-    coatColor: WIZARD_COATS_COLORS[randomIndex],
-    eyesColor: WIZARD_EYES_COLORS[randomIndex]
-  },
-  {
-    name: WIZARD_NAMES[randomIndex],
-    coatColor: WIZARD_COATS_COLORS[randomIndex],
-    eyesColor: WIZARD_EYES_COLORS[randomIndex]
-  },
-  {
-    name: WIZARD_NAMES[randomIndex],
-    coatColor: WIZARD_COATS_COLORS[randomIndex],
-    eyesColor: WIZARD_EYES_COLORS[randomIndex]
-  },
-  {
-    name: WIZARD_NAMES[randomIndex],
-    coatColor: WIZARD_COATS_COLORS[randomIndex],
-    eyesColor: WIZARD_EYES_COLORS[randomIndex]
-  }
-];
+  return arr[randomIndex];
+};
 
-// Функция случайной генерации
-// var getRandom = function (wizardObjectProperty) {
-//   var randomIndex = Math.floor(Math.random() * WIZARD_NAMES.length);
-//   var randomWizadName = WIZARD_NAMES[randomIndex];
-// };
+// Функция генерации магов в массив объектов
+var createWizards = function (count) {
+  var wizards = [];
+
+  for (var i = 0; i < count; i++) {
+    wizards[i].push(getRandomElement(WIZARD_NAMES + '' + WIZARD_SURNAMES),
+                    getRandomElement(WIZARD_COATS_COLORS),
+                    getRandomElement(WIZARD_EYES_COLORS));
+
+
+};
 
 // Функция отрисовки мага
 var renderWizard = function (wizard) {
