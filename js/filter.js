18de814c8812fd2ee00ce0c083261876a050a05f
DanSummer <dan.summer363@gmail.com>
2019-10-24 19:43:24 +0300

    Исправление ошибки наименования переменной

--- js/filter.js
+++ js/filter.js
@@ -24,10 +24,10 @@
   // Функция отрисовки похожих, отфильтрованных магов
   var updateWizards = function () {
     window.createWizard.renderSimilarWizards(wizards.slice().
-      sort(function (left, rigth) {
-        var rankDiff = getRank(rigth) - getRank(left);
+      sort(function (left, right) {
+        var rankDiff = getRank(right) - getRank(left);
         if (rankDiff === 0) {
-          rankDiff = wizards.indexOf(left) - wizards.indexOf(rigth);
+          rankDiff = wizards.indexOf(left) - wizards.indexOf(right);
         }
 
         return rankDiff;
